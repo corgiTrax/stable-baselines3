ce4088f8586bece58d8a2c27a1dda4d3e0f8aa4c
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-01-24 11:51:23 -0800

    Experiment 32 results

--- stable_baselines3/active_tamer/tamer_sac_optim.py
+++ stable_baselines3/active_tamer/tamer_sac_optim.py
@@ -110,7 +110,7 @@ class TamerSACOptim(OffPolicyAlgorithm):
         _init_setup_model: bool = True,
         model_name: str = "TamerSACOptim",
         render: bool = False,
-        q_val_threshold: float = 0.05
+        q_val_threshold: float = 0.99
     ):
 
         super(TamerSACOptim, self).__init__(
