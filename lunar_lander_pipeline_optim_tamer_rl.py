13628a353526ebf0d5fa1985fc509d1d002348cc
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2022-02-24 11:01:13 -0800

    All results from tamer + rl

--- lunar_lander_pipeline_optim_tamer_rl.py
+++ lunar_lander_pipeline_optim_tamer_rl.py
@@ -82,7 +82,7 @@ def main():
         seed=config_data["seed"],
         render=False,
         trained_model=trained_model,
-        percent_feedback=1.0,
+        percent_feedback=0.01,
     )
 
     print(f"Model Policy = " + str(model.policy))
