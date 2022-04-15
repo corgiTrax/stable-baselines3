c056027bef64a4129765325180b2bdeab09ef9e6
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-04-15 10:31:14 -0700

    TamerRL bugfixes

--- stable_baselines3/active_tamer/active_tamerRL_sac_optim.py
+++ stable_baselines3/active_tamer/active_tamerRL_sac_optim.py
@@ -582,9 +582,8 @@ class ActiveTamerRLSACOptim(OffPolicyAlgorithm):
                     th.from_numpy(prev_obs).to(self.device).reshape(1, -1)
                 )
                 if (
-                    (scene_graph_updated or
-                    state_prediction_err > self.prediction_threshold) and
-                    self.num_timesteps > 10000
+                    scene_graph_updated or
+                    state_prediction_err > self.prediction_threshold
                     # state_reconstructor_err > self.prediction_threshold
                 ):
                     simulated_human_reward = (
