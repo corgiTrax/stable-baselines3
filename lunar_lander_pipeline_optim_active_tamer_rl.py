463f7e9cdd733c007c57639b8e724f0b9745fa9f
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-05-03 22:10:28 -0700

    Removing lateral engine states

--- lunar_lander_pipeline_optim_active_tamer_rl.py
+++ lunar_lander_pipeline_optim_active_tamer_rl.py
@@ -102,7 +102,7 @@ class LunarLanderSceneGraph:
     def getCurrGraph(self):
         self.curr_graph = [self.isLeft(self.agent, self.flag1), self.isLeft(self.agent, self.flag2), self.isLeft(self.agent, self.mountain),
                 self.onTop(self.agent, self.flag1), self.onTop(self.agent, self.flag2), self.onTop(self.agent, self.mountain), 
-                self.main_engine_on(self.agent), self.left_engine_on(self.agent), self.right_engine_on(self.agent), self.is_upright(self.agent)]
+                self.main_engine_on(self.agent), self.is_upright(self.agent)]
         # self.curr_graph = [self.isLeft(self.agent, self.mountain), self.onTop(self.agent, self.mountain), self.midway(self.agent), self.oob_left(self.agent), self.oob_right(self.agent), self.oob_top(self.agent)]
         self.state_counts[tuple(self.curr_graph)] += 1
         self.max_counts = max(self.max_counts, self.state_counts[tuple(self.curr_graph)])
