e651b21570fb7ba70700370e89afecdc371caa41
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-05-08 23:17:55 -0700

    modifying proportion

--- lunar_lander_pipeline_optim_active_tamer_rl.py
+++ lunar_lander_pipeline_optim_active_tamer_rl.py
@@ -79,7 +79,10 @@ class LunarLanderSceneGraph:
         return rank
     
     def calculate_ucb(self, graph):
-        return self.human_critic_average[tuple(graph)] + 0.2 * math.sqrt(2 * self.given_feedback / self.state_counts[tuple(graph)])       
+        # file1 = open("ucb_proportions.txt", "a")
+        # file1.write(f'{self.human_critic_average[tuple(graph)]}   {0.2 * math.sqrt(2 * self.given_feedback / self.state_counts[tuple(graph)])}\n')
+        # file1.close() 
+        return self.human_critic_average[tuple(graph)] + 0.02 * math.sqrt(2 * self.given_feedback / self.state_counts[tuple(graph)])       
     
     def getUCBRank(self):
         curr_graph_ucb1 = self.calculate_ucb(self.curr_graph)
