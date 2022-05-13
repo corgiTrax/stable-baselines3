d19120ba429b73f7e002bcc18dee4a6ee9943e0f
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-05-12 21:21:24 -0700

    Bugfix: use ucb rank not state change

--- stable_baselines3/active_tamer/active_tamerRL_sac_optim.py
+++ stable_baselines3/active_tamer/active_tamerRL_sac_optim.py
@@ -592,10 +592,10 @@ class ActiveTamerRLSACOptim(OffPolicyAlgorithm):
                     th.from_numpy(prev_obs).to(self.device).reshape(1, -1)
                 )
                 if (
-                    scene_graph_updated
+                    # scene_graph_updated
                     # random.random() < curr_state_prob  
                     # unfamiliar_state
-                    # ucb_rank_high
+                    ucb_rank_high
                     # state_prediction_err > self.prediction_threshold
                     #  state_reconstructor_err > self.prediction_threshold
                 ):
