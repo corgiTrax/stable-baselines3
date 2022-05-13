72ea1f36c362847e6a9cb43e222289ddd6ecce90
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-05-12 17:31:02 -0700

    Increasing weight of ucb and increasing lower limit

--- lunar_lander_pipeline_optim_active_tamer_rl.py
+++ lunar_lander_pipeline_optim_active_tamer_rl.py
@@ -163,7 +163,7 @@ class LunarLanderSceneGraphV2:
         # file1 = open("ucb_proportions.txt", "a")
         # file1.write(f'{self.aRPE_average[tuple(graph)]}   {0.01 * math.sqrt(2 * self.given_feedback / (self.num_feedback_given[tuple(graph)] + 1))}\n')
         # file1.close()+
-        return self.aRPE_average[tuple(graph)] + 0.1 * math.sqrt(2 * self.given_feedback / (self.num_feedback_given[tuple(graph)] + 1))       
+        return self.aRPE_average[tuple(graph)] + 0.2 * math.sqrt(2 * self.given_feedback / (self.num_feedback_given[tuple(graph)] + 1))       
     
     def getUCBRank(self):
         curr_graph_ucb1 = self.calculate_ucb(self.curr_graph)
@@ -216,7 +216,7 @@ class LunarLanderSceneGraphV2:
         self.agent['action'] = {'down': action[0][0], 'lateral': action[0][0]}
         self.agent['orientation'] = newState[0][4]
         self.total_timesteps += 1
-        return self.getCurrGraph() != prev_graph, self.getUCBRank() <= 45
+        return self.getCurrGraph() != prev_graph, self.getUCBRank() <= 60
 
 
 def train_model(model, config_data, feedback_gui, human_feedback, env):
