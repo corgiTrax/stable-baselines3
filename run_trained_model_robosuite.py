cec4b5842635c5ebb1b5b8ad9626ff0fd7347440
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-05-22 14:44:41 -0700

    udpated sac model

--- run_trained_model_robosuite.py
+++ run_trained_model_robosuite.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     trained_model = SAC.load(
-        "models/SAC_144000.pt", env, custom_objects=custom_objects, **kwargs
+        "models/SAC_150000.pt", env, custom_objects=custom_objects, **kwargs
     )
     # do visualization
     for i in range(10000):
