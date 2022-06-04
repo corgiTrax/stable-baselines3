033fd60a59b16a8c174b37e3845b9ddad6ea1dc1
brainiac-ws <brainiac@brainiac-ws>
2022-06-03 20:20:54 -0700

    fixed real robot reset bug

--- real_sawyer_env.py
+++ real_sawyer_env.py
@@ -17,13 +17,15 @@ from stable_baselines3.active_tamer.sac import SAC
 
 # perls2 modules
 from demos.sawyer_osc_2d import OpSpaceLineXYZ
+from perls2.envs.env import Env
 
 
-class RealSawyerReachingEnv(gym.Env):
+class RealSawyerReachingEnv3d(gym.Env):
     metadata = {'render.modes':['human']}
 
     def __init__(self, driver):
-        super(RealSawyerReachingEnv, self).__init__()
+        # raise NotImplementedError()
+        super(RealSawyerReachingEnv3d, self).__init__()
 
         self.action_dim = 4 # action = [dx, dy]
         self.obs_dim = 2 # observation  = [eef_x, eef_y]
@@ -57,16 +59,18 @@ class RealSawyerReachingEnv(gym.Env):
         # workspace boundaries (low, high)
         # x bondary: 0.900 0.832 0.345 0.319
         # y boundary: -0.171 0.486 -0.178 0.467 
-        self.x_lim = np.array([0.319, 0.832]) - self.origin 
-        self.y_lim = np.array([-0.178, 0.467]) - self.origin
+        self.x_lim = np.array([0.319, 0.775]) - self.origin 
+        self.y_lim = np.array([-0.160, 0.467]) - self.origin
+        # TODO - z-boundary
 
+        # TODO
         # Target boundaries
         # x boundary: 0.671 0.748 0.746 0.667
         # y boundary: 0.276 0.272 0.040 0.046
         self.target_x = np.array([0.671, 0.746]) - self.origin
         self.target_y =  np.array([0.046, 0.272]) - self.origin
 
-    def reward(self):
+    def reward(self): # TODO
         
         eef_xpos = self.get_state()
         x_in_target = self.target_x[0] < eef_xpos[0] < self.target_x[1]
@@ -75,7 +79,7 @@ class RealSawyerReachingEnv(gym.Env):
         return int(x_in_target and y_in_target)
 
 
-    def get_state(self):
+    def get_state(self): # TODO
 
         return self.driver.get_eef_xy() - self.origin
 
@@ -85,11 +89,11 @@ class RealSawyerReachingEnv(gym.Env):
         # scale input to controller
         # action = self.ctrl_scale * action # for 2d action space model
         sf = 1.5 # safety factor for boundary limit
-        action = self.ctrl_scale * action[:2] # for 4d action space model
+        action = self.ctrl_scale * action[:3] # for 4d action space model
 
         # check workspace boundary condition
         eef_xpos = self.get_state()
-        print(f'Current position = {str(eef_xpos)}')
+        # print(f'Current position = {str(eef_xpos)}')
         x_in_bounds = self.x_lim[0] < eef_xpos[0] + action[0] < self.x_lim[1]
         y_in_bounds = self.y_lim[0] < eef_xpos[1] + action[1] < self.y_lim[1]
         
@@ -107,6 +111,7 @@ class RealSawyerReachingEnv(gym.Env):
 
         if reward > 0:
             # task is completed
+            print("---------TARGET REACHED-----------")
             done = True
 
         if self.steps > self.max_steps:
@@ -144,3 +149,309 @@ class RealSawyerReachingEnv(gym.Env):
 
     def close(self):
         self.close()
+
+class RealSawyerReachingEnv(Env):
+    metadata = {'render.modes':['human']}
+
+    def __init__(self, driver, random_init=True):
+        super().__init__(
+            config = '/home/robot/perls2/demos/demo_control_cfg.yaml',
+        )
+
+        self.action_dim = 4 # action = [dx, dy]
+        self.obs_dim = 2 # observation  = [eef_x, eef_y]
+
+        # define action space
+        # self.action_space = spaces.Box(low=-0.1, high=0.1, shape=(self.action_dim, ))#, dtype=np.float32)
+        self.action_space = spaces.Box(
+            low=np.array([-0.25, -0.25, -0.25, -1.0]),
+            high=np.array([0.25, 0.25, 0.25, 1.0]),
+            shape=(4,),
+            dtype=np.float32)
+
+        # define observation space
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.obs_dim, ))#, dtype=np.float32)
+
+        # define driver - perls2 controller, rewards, etc.
+        self.driver = driver
+
+        # max time steps
+        self.max_steps = 900
+        self.steps = 0 
+
+        # scaling factor from action -> osc control command
+        self.ctrl_scale = 0.075
+
+        # world origin (table center)
+        self.origin = np.array([0.7075, 0.150])
+
+        # workspace boundaries (low, high)
+        # x bondary: 0.900 0.832 0.345 0.319
+        # y boundary: -0.171 0.486 -0.178 0.467 
+        self.x_lim = np.array([0.319, 0.775]) - self.origin 
+        self.y_lim = np.array([-0.160, 0.467]) - self.origin
+
+        # Target boundaries
+        # x boundary: 0.671 0.748 0.746 0.667
+        # y boundary: 0.276 0.272 0.040 0.046
+        self.target_x = np.array([0.671, 0.746]) - self.origin
+        self.target_y =  np.array([0.046, 0.272]) - self.origin
+
+        # Whether to use random position initialization
+        self.random_init = random_init
+
+        # Initial position
+        self.init_state = np.array([-0.25, 0])
+
+        # print("------NEUTRAL POS", self.robot_interface.limb_neutral_positions)
+        # print("------NEUTRAL EEF POS", self.robot_interface.ee_position)
+        # print("------NEUTRAL EEF ORI", self.robot_interface.ee_orientation)
+
+        # print("--------INITIAL POS", self.get_state())
+
+
+
+
+
+    def reward(self):
+        
+        eef_xpos = self.get_state()
+        x_in_target = self.target_x[0] < eef_xpos[0] < self.target_x[1]
+        y_in_target = self.target_y[0] < eef_xpos[1] < self.target_y[1]
+
+        return int(x_in_target and y_in_target)
+
+
+    def get_state(self):
+
+        return self.driver.get_eef_xy() - self.origin
+
+
+    def step(self, action):
+
+        # scale input to controller
+        # action = self.ctrl_scale * action # for 2d action space model
+        sf = 1.5 # safety factor for boundary limit
+        action = self.ctrl_scale * action[:2] # for 4d action space model
+
+        # check workspace boundary condition
+        eef_xpos = self.get_state()
+        # print(f'Current position = {str(eef_xpos)}')
+        x_in_bounds = self.x_lim[0] < eef_xpos[0] + action[0] < self.x_lim[1]
+        y_in_bounds = self.y_lim[0] < eef_xpos[1] + action[1] < self.y_lim[1]
+        
+        if not x_in_bounds or not y_in_bounds:
+            # if next action will send eef out of boundary, ignore the action
+            print("action out of bounds")
+            action = np.zeros(self.action_dim)
+
+        self.driver.step_axis(action) # take action
+        observation = self.get_state() # observation = [eef_x, eef_y]
+        reward = self.reward()
+        
+        # done if:
+        done = False
+
+        if reward > 0:
+            # task is completed
+            print("---------TARGET REACHED-----------")
+            done = True
+
+        if self.steps > self.max_steps:
+            # max steps is reached
+            done = self.steps > self.max_steps # finish if reached maximum time steps
+        
+        info = {} 
+
+        self.steps += 1
+
+        return observation, reward, done, info
+
+    def _move_to_initial_pos(self):
+
+        thresh = 0.01
+        print("-------initial state", self.init_state)
+        print("-------curr state", self.get_state())
+        print(self.init_state - self.get_state())
+
+        vec = self.init_state - self.get_state()
+        while (abs(vec[0]) > thresh) or (abs(vec[1]) > thresh):
+            action = self.init_state - self.get_state() # vector from current to initial position
+            print("-----RESET ACTION", action)
+            self.step(action)
+            vec = self.init_state - self.get_state()
+            
+    
+    def reset(self):
+
+        print("----------------Resetting-----------------")
+
+        self._move_to_initial_pos()
+        print("-----Moved to initial pos---------")
+        time.sleep(3)
+
+        observation = self.get_state() # update observation
+        print("Observation", observation)
+
+        # random eef position intialization
+        if self.random_init:
+            action_x = np.random.uniform(-0.015, 0.035, 1)
+            action_y = np.random.uniform(-0.070, 0.070, 1)
+            action = np.concatenate([action_x, action_y, np.zeros(2)])
+            print(f"-----Action is {action}------")
+            for i in range(50):
+                observation, _, _, _ = self.step(action)
+        
+        self.steps = 0
+
+        print("--------------Finished Resetting. Starting in 3 sec-----------")
+        time.sleep(3)
+
+        return observation
+
+    def render(self, mode='human'):
+        return
+
+    def close(self):
+        self.close()
+
+
+# class RealSawyerReachingEnv(gym.Env):
+#     metadata = {'render.modes':['human']}
+
+#     def __init__(self, driver, random_init=True):
+#         super(RealSawyerReachingEnv, self).__init__()
+
+#         self.action_dim = 4 # action = [dx, dy]
+#         self.obs_dim = 2 # observation  = [eef_x, eef_y]
+
+#         # define action space
+#         # self.action_space = spaces.Box(low=-0.1, high=0.1, shape=(self.action_dim, ))#, dtype=np.float32)
+#         self.action_space = spaces.Box(
+#             low=np.array([-0.25, -0.25, -0.25, -1.0]),
+#             high=np.array([0.25, 0.25, 0.25, 1.0]),
+#             shape=(4,),
+#             dtype=np.float32)
+
+#         # define observation space
+#         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.obs_dim, ))#, dtype=np.float32)
+
+#         # define driver - perls2 controller, rewards, etc.
+#         self.driver = driver
+
+#         # max time steps
+#         self.max_steps = 900
+#         self.steps = 0 
+
+#         # scaling factor from action -> osc control command
+#         self.ctrl_scale = 0.075
+
+#         # world origin (table center)
+#         self.origin = np.array([0.7075, 0.150])
+
+#         # workspace boundaries (low, high)
+#         # x bondary: 0.900 0.832 0.345 0.319
+#         # y boundary: -0.171 0.486 -0.178 0.467 
+#         self.x_lim = np.array([0.319, 0.775]) - self.origin 
+#         self.y_lim = np.array([-0.160, 0.467]) - self.origin
+
+#         # Target boundaries
+#         # x boundary: 0.671 0.748 0.746 0.667
+#         # y boundary: 0.276 0.272 0.040 0.046
+#         self.target_x = np.array([0.671, 0.746]) - self.origin
+#         self.target_y =  np.array([0.046, 0.272]) - self.origin
+
+#         # Whether to use random position initialization
+#         self.random_init = random_init
+
+
+
+#     def reward(self):
+        
+#         eef_xpos = self.get_state()
+#         x_in_target = self.target_x[0] < eef_xpos[0] < self.target_x[1]
+#         y_in_target = self.target_y[0] < eef_xpos[1] < self.target_y[1]
+
+#         return int(x_in_target and y_in_target)
+
+
+#     def get_state(self):
+
+#         return self.driver.get_eef_xy() - self.origin
+
+
+#     def step(self, action):
+
+#         # scale input to controller
+#         # action = self.ctrl_scale * action # for 2d action space model
+#         sf = 1.5 # safety factor for boundary limit
+#         action = self.ctrl_scale * action[:2] # for 4d action space model
+
+#         # check workspace boundary condition
+#         eef_xpos = self.get_state()
+#         # print(f'Current position = {str(eef_xpos)}')
+#         x_in_bounds = self.x_lim[0] < eef_xpos[0] + action[0] < self.x_lim[1]
+#         y_in_bounds = self.y_lim[0] < eef_xpos[1] + action[1] < self.y_lim[1]
+        
+#         if not x_in_bounds or not y_in_bounds:
+#             # if next action will send eef out of boundary, ignore the action
+#             print("action out of bounds")
+#             action = np.zeros(self.action_dim)
+
+#         self.driver.step_axis(action) # take action
+#         observation = self.get_state() # observation = [eef_x, eef_y]
+#         reward = self.reward()
+        
+#         # done if:
+#         done = False
+
+#         if reward > 0:
+#             # task is completed
+#             print("---------TARGET REACHED-----------")
+#             done = True
+
+#         if self.steps > self.max_steps:
+#             # max steps is reached
+#             done = self.steps > self.max_steps # finish if reached maximum time steps
+        
+#         info = {} 
+
+#         self.steps += 1
+
+#         return observation, reward, done, info
+
+    
+#     def reset(self):
+
+#         print("----------------Resetting-----------------")
+#         # self.driver.connect_and_reset_robot() # move to home position?
+
+
+#         observation = self.get_state() # update observation
+#         print("Observation", observation)
+
+#         # random eef position intialization
+#         if self.random_init:
+#             action_x = np.random.uniform(-0.02, 0.04, 1)
+#             action_y = np.random.uniform(-0.075, 0.075, 1)
+#             action = np.concatenate([action_x, action_y, np.zeros(2)])
+#             print(f"-----Action is {action}------")
+#             for i in range(50):
+#                 # action_x = np.random.uniform(-0.02, 0.04, 1)
+#                 # action_y = np.random.uniform(-0.075, 0.075, 1)
+#                 # action = np.concatenate([action_x, action_y, np.zeros(2)])
+#                 # action = np.random.uniform(-0.25, 0.25, 4)
+#                 observation, _, _, _ = self.step(action)
+        
+#         self.steps = 0
+
+#         print("--------------Finished Resetting. Starting in 3 sec-----------")
+#         time.sleep(3)
+
+#         return observation
+
+#     def render(self, mode='human'):
+#         return
+
+#     def close(self):
+#         self.close()
--- sawyer_rl_driver.py
+++ sawyer_rl_driver.py
@@ -17,7 +17,7 @@ from stable_baselines3.active_tamer.sac import SAC
 # perls2 modules
 from demos.sawyer_osc_2d import OpSpaceLineXYZ
 
-from real_sawyer_env import RealSawyerReachingEnv
+from real_sawyer_env import RealSawyerReachingEnv, RealSawyerReachingEnv3d
 
 def run_pretrained(env):
     ### Run pre-trained model ####
@@ -81,11 +81,11 @@ def test_sound(env):
         obs, reward, done, _ = env.step(action)
         # env.render()
 
-def record_state(env):
+def record_state(env, axis=0):
     ### Record State ###
     states = []
-    actions = np.loadtxt("actions.txt", delimiter=',')
-    robosuite_states = np.loadtxt("robosuite_states.txt", delimiter=',')
+    actions = np.loadtxt(f"actions{axis}.txt", delimiter=',')
+    robosuite_states = np.loadtxt(f"robosuite_states{axis}.txt", delimiter=',')
     for i in range(actions.shape[0]):
         action = actions[i]
         obs, _, _, _ = env.step(action)
@@ -93,7 +93,7 @@ def record_state(env):
 
     states = np.array(states)
 
-    np.savetxt("states.txt", states, delimiter=',', newline='\n', fmt='%1.5f')
+    np.savetxt(f"sawyer_states{axis}.txt", states, delimiter=',', newline='\n', fmt='%1.5f')
     print("-------Finished Actions-------")
     error = np.mean(states - robosuite_states, axis=0)
     states_shifted = states - error
@@ -102,14 +102,57 @@ def record_state(env):
 
 def calibrate_boundary_helper(env):
     while True:
-        # print("raw", driver.get_eef_xy())
-        print("calib", driver.get_eef_xy() - env.origin)
+        print("raw", driver.get_eef_xy())
+        # print("calib", driver.get_eef_xy() - env.origin)
 
 
 if __name__ == "__main__":
 
     print("\n\n-----------------[0]------------------")
-        
+    
+    
+    # parser = argparse.ArgumentParser(
+    #     description="Test controllers and measure errors.")
+    # parser.add_argument('--world', default=None, help='World type for the demo, uses config file if not specified', choices=['Bullet', 'Real'])
+    # parser.add_argument('--robot', default='sawyer', help='Robot type overrides config', choices=['panda', 'sawyer'])
+    # parser.add_argument('--ctrl_type',
+    #                     default="EEImpedance",
+    #                     help='Type of controller to test')
+    # parser.add_argument('--demo_type',
+    #                     default="Line",
+    #                     help='Type of menu to run.')
+    # parser.add_argument('--test_fn',
+    #                     default='set_ee_pose',
+    #                     help='Function to test',
+    #                     choices=['set_ee_pose', 'move_ee_delta', 'set_joint_delta', 'set_joint_positions', 'set_joint_torques', 'set_joint_velocities'])
+    # parser.add_argument('--path_length', type=float,
+    #                     default=None, help='length in m of path')
+    # parser.add_argument('--delta_val',
+    #                     default=[0.001, 0.001, 0.001], type=float,
+    #                     help="Max step size (m or rad) to take for demo.")
+    # parser.add_argument('--axis',
+    #                     default='x', type=str,
+    #                     choices=['x', 'y', 'z'],
+    #                     help='axis for demo. Position direction for Line or rotation axis for Rotation')
+    # parser.add_argument('--num_steps', default=1, type=int,
+    #                     help="max steps for demo.")
+    # parser.add_argument('--plot_pos', action="store_true",
+    #                     help="whether to plot positions of demo.")
+    # parser.add_argument('--plot_error', action="store_true",
+    #                     help="whether to plot errors.")
+    # parser.add_argument('--save', action="store_true",
+    #                     help="whether to store data to file")
+    # parser.add_argument('--demo_name', default=None,
+    #                     type=str, help="Valid filename for demo.")
+    # parser.add_argument('--save_fig', action="store_true",
+    #                     help="whether to save pngs of plots")
+    # parser.add_argument('--fix_ori', action="store_true", default=True,
+    #                     help="fix orientation for move_ee_delta")
+    # parser.add_argument('--fix_pos', action="store_true",
+    #                     help="fix position for move_ee_delta")
+    # parser.add_argument('--config_file', default='/home/robot/perls2/demos/demo_control_cfg.yaml', help='absolute filepath for config file.')
+    # parser.add_argument('--cycles', type=int, default=1, help="num times to cycle path (only for square)")
+
     parser = argparse.ArgumentParser(
         description="Test controllers and measure errors.")
     parser.add_argument('--world', default=None, help='World type for the demo, uses config file if not specified', choices=['Bullet', 'Real'])
@@ -151,36 +194,38 @@ if __name__ == "__main__":
                         help="fix position for move_ee_delta")
     parser.add_argument('--config_file', default='/home/robot/perls2/demos/demo_control_cfg.yaml', help='absolute filepath for config file.')
     parser.add_argument('--cycles', type=int, default=1, help="num times to cycle path (only for square)")
+    
     args = parser.parse_args()
     kwargs = vars(args)
 
     driver = OpSpaceLineXYZ(**kwargs)
 
-    env = RealSawyerReachingEnv(driver)
+    env = RealSawyerReachingEnv(driver, random_init=True)
+    # env = RealSawyerReachingEnv3d(driver)
 
+    for i in range(5):
+        print(f"Reset {i}")
+        env.reset()
     # check_env(env)
 
-    # record_state(env)
-    calibrate_boundary_helper(env)
+    # record_state(env, axis=0)
+    # calibrate_boundary_helper(env)
             
 
-
-    ###### Test perls2 driver ######
-
-
-
-    #### Move to target ####
-    # for i in range(20):
+    # ### Motion Test ####
+    # for i in range(50):
     #     print("step ", i)
-    #     driver.step_axis([0.00, -0.005])
+    #     action = np.array([-0.02, 0, 0, 0])
+    #     env.step(action)
         
-    #     if driver.reward():
-    #         print("----Target Reached-----")
-    #         break
+        # if driver.reward():
+        #     print("----Target Reached-----")
+        #     break
 
-    # for i in range(150):
+    # for i in range(50):
     #     print("step ", i)
-    #     driver.step_axis([0.003, 0.00])
+    #     action = np.array([0, 0.075, 0, 0])
+    #     env.step(action)
         
     #     if driver.reward():
     #         print("----Target Reached-----")
