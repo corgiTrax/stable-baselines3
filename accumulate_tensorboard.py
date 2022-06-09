9fbba896289e62342b5267fe618585c1d6cf1cf3
Dhruva Bansal <dhruva.bansal00@gmail.com>
2022-06-10 03:44:06 +0530

    Local repo update

--- accumulate_tensorboard.py
+++ accumulate_tensorboard.py
@@ -59,8 +59,7 @@ def average_from_per_episode_rewards(dpath, target_len):
     for index, curr_iterator in enumerate(summary_iterators):        
         for iter_index, i in enumerate(curr_iterator.Scalars(tag)):
             if i.step < target_len:
-                curr_rewards = i.value * len(rewards_buffer)
-                print(i.step)
+                curr_rewards = i.value * len(rewards_buffer) * 4
                 curr_steps = i.step * len(steps_buffer)
                 
                 if len(rewards_buffer) == 25:
@@ -68,8 +67,6 @@ def average_from_per_episode_rewards(dpath, target_len):
                     steps_buffer.pop(0)
                 
                 curr_rewards -= sum(rewards_buffer)
-                if curr_rewards < 0:
-                    print(curr_rewards)
                 curr_steps -= sum(steps_buffer)
                 data[target_tag].append((curr_steps + data[target_tag][-1][0], curr_rewards + data[target_tag][-1][1], iter_index + 1))
 
@@ -80,6 +77,31 @@ def average_from_per_episode_rewards(dpath, target_len):
         if i.step < target_len:
             out[target_tag].append(data[target_tag][index])
             steps[target_tag].append(i.step)
+    
+    return out, steps
+
+def extract_feedback_curves(dpath, target_len):
+
+    summary_iterators = [EventAccumulator(os.path.join(dpath)).Reload()]
+    tags = summary_iterators[0].Tags()['scalars']
+
+    for it in summary_iterators:
+        assert it.Tags()['scalars'] == tags
+
+    out = defaultdict(list)
+    steps = defaultdict(list)
+    data = defaultdict(list)
+    tag = 'train/feedback_percentage'
+
+    for index, curr_iterator in enumerate(summary_iterators):        
+        for iter_index, i in enumerate(curr_iterator.Scalars(tag)):
+            if i.step < target_len:
+                data[tag].append((i.step, i.value, iter_index + 1))
+    
+    for index, i in enumerate(summary_iterators[0].Scalars(tag)):
+        if i.step < target_len:
+            out[tag].append(data[tag][index])
+            steps[tag].append(i.step)
     
     return out, steps
 
@@ -98,13 +120,15 @@ def write_to_csv(data, steps, csv_name):
         writer.writerows(rows)
         
 
-dpath = 'reaching_task_results_all/all_runs/RPEActiveTamer'
+dpath = 'ballbasket_task_results_all/all_runs/Tamer100'
 
 for dname in os.listdir(dpath):
     # data, steps = average_from_per_timestep_rewards(os.path.join(dpath, dname), 150000)
     # update_existing_tensorboard(os.path.join(dpath, dname), 'train/training_rewards_cumulative', steps, data)
     if ".csv" not in dname:
-        print(dname)
         data, steps = average_from_per_episode_rewards(os.path.join(dpath, dname), 100000)
         write_to_csv(data['rollout/training_rewards_cumulative'], steps['rollout/training_rewards_cumulative'], os.path.join(dpath, dname+".csv"))
         # update_existing_tensorboard(os.path.join(dpath, dname), 'rollout/training_rewards_cumulative', steps, data)
+
+        curr_feedback_data, steps = extract_feedback_curves(os.path.join(dpath, dname), 100000)
+        write_to_csv(curr_feedback_data['train/feedback_percentage'], steps['train/feedback_percentage'], os.path.join(dpath, dname+"_feedback.csv"))
